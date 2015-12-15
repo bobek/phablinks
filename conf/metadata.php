e77eb4fd4b9463a0d548cdd9397a582bb67a9c5e
Antonin Kral <a.kral@bobek.cz>
2015-12-15 13:36:10 +0100

    Fix incorrect configuration option

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Configuration metadata for the Bugzilla-links plugin
+ * Configuration metadata for the Phab-links plugin
  *
  * @author christian studer <christian.studer@meteotest.ch>
  */
-$meta['bugzillaserver'] = array('string');
+$meta['phabserver'] = array('string');
